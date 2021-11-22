b3dd1acaa1cea494f88dda708562675f496da185
Katelyn <katelynjewel@gmail.com>
2021-11-22 15:16:23 -0800

    commit tree 4b595987708c480db3dd8b122c764e15d8941ed7

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,2 +1,20 @@
 class Post < ApplicationRecord
+    validates :title, presence: true
+    validates :content, length: { minimum: 250 }
+    validates :summary, length: { maximum: 250 }
+    validates :category, inclusion: { in: ['Fiction', 'Non-Fiction'] }
+    validate :click_bait
+
+
+
+    def click_bait
+        if title 
+            unless ["Won't Believe", "Secret", "Top [number]", "Guess"].any?{ |phrase| title.include? phrase }
+                errors.add(:title, "Title needs to be better" )
+            end
+        end
+    end
+
 end
+
+
